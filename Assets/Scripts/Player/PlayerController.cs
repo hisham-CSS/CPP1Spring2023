ba50027c7970986213ab389d08590982d9b9a56c
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2023-04-25 11:10:36 -0400

    Lab 3 incomplete

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    //Component references
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+
+    //movement var
+    public float speed = 5.0f;
+    public float jumpForce = 300.0f;
+
+    //groundcheck stuff
+    public bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask isGroundLayer;
+    public float groundCheckRadius = 0.02f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        if (speed <= 0) speed = 5.0f;
+        if (jumpForce <= 0) jumpForce = 300.0f;
+        if (groundCheckRadius <= 0) groundCheckRadius = 0.02f;
+
+        if (!groundCheck) groundCheck = GameObject.FindGameObjectWithTag("GroundCheck").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+        rb.velocity = moveDirection;
+
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+    }
+}
